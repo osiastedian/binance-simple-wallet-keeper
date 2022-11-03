11a60cf1f99addb62963e38aad7af20e6406b9d0
Ted Ian Osias <osiastedian@gmail.com>
2022-11-03 10:29:21 +0800

    feat: save to localstorage

--- src/store/Provider.tsx
+++ src/store/Provider.tsx
@@ -3,11 +3,27 @@ import { ReactNode } from "react";
 import { Provider } from "react-redux";
 import WalletSlice from "./wallets";
 
+const LOCALSTORAGE_KEY = "STORE_STATE";
+
+const persistedStateSerialized = localStorage.getItem(LOCALSTORAGE_KEY);
+
+let preloadedState = persistedStateSerialized
+  ? JSON.parse(persistedStateSerialized)
+  : undefined;
+
 const store = configureStore({
   reducer: {
     wallet: WalletSlice.reducer,
   },
+  preloadedState,
 });
+
+const saveStoreSate = () => {
+  const serialized = JSON.stringify(store.getState());
+  localStorage.setItem(LOCALSTORAGE_KEY, serialized);
+};
+
+store.subscribe(() => saveStoreSate());
 
 const StoreProvider: React.FC<{ children: ReactNode }> = ({ children }) => (
   <Provider store={store}>{children}</Provider>
